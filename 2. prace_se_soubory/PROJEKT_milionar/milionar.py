98323c2a00caa16eece6bce704dd6d1d02d7dfcd
Hery387 <medved.hery@gmail.com>
2025-12-12 11:37:17 +0100

    +Added the game function |Fixed the winners function

--- 2. prace_se_soubory/PROJEKT_milionar/milionar.py
+++ 2. prace_se_soubory/PROJEKT_milionar/milionar.py
@@ -4,6 +4,7 @@ import time
 import csv
 import matplotlib.pyplot as plt
 from collections import Counter
+import random
 
 def clear_terminal():
     if os.name == "nt":
@@ -84,6 +85,9 @@ def login():
                 print("Login successful!")
                 time.sleep(2)
                 return logged_in(input_username)
+    print("Your username or password is wrong!")
+    time.sleep(2)
+    return menu()
             
 def logged_in(username):
     clear_terminal()
@@ -100,7 +104,7 @@ def logged_in(username):
     elif choice == "2":
         winners(username)
     elif choice == "3":
-        pass
+        game(username)
     elif choice == "4":
         menu()
     else:
@@ -115,7 +119,6 @@ def graph_statistics(username):
     clear_terminal()
 
     question_topic = []
-    question_difficulty = []
     question_easy = 0
     question_medium = 0
     question_hard = 0
@@ -125,7 +128,6 @@ def graph_statistics(username):
         next(reader)
         for row in reader:
             question_topic.append(row[2])
-            question_difficulty.append(row[1])
             if row[1] == "easy":
                 question_easy += 1
             elif row[1] == "medium":
@@ -162,14 +164,25 @@ def graph_statistics(username):
     return logged_in(username)
 
 def winners(username):
+    clear_terminal()
+    wins = {}
     try:
         with open(r'2. prace_se_soubory\PROJEKT_milionar\winners_database.csv', 'r', encoding='utf-8', newline='') as file:
             reader = csv.reader(file)
+            next(reader)
             for row in reader:
-                print(f"User {row[1]} has won {row[0]} times!")
-            while True:
-                if input("Type 'exit' to return to menu: ").lower() == 'exit':
-                    return logged_in(username)
+                if row[1] not in wins:
+                    wins[row[1]] = 1
+                else:
+                    wins[row[1]] += 1
+
+        for player in wins:
+            print(f"User {player} has won {wins[player]} times!")
+        
+        print()
+        while True:
+            if input("Type 'exit' to return to menu: ").lower() == 'exit':
+                return logged_in(username)
     except FileNotFoundError:
         print("No one has won yet!")
         time.sleep(2)
@@ -196,9 +209,90 @@ def add_winner(username):
         for row in reader:
             if row[1] == username:
                 win_count += 1
+    clear_terminal()
     print(f"You have won {win_count} times!")
-    time.sleep(2)
+    time.sleep(3)
     
     return logged_in(username)
+
+def game(username):
+    easy_questions = {}
+    medium_questions = {}
+    hard_questions = {}
+    playing = True
+    should_win = False
+    question_count = 0
+
+    clear_terminal()
+    print(f"Welcome, {username} to the Cichna wants to be a millionaire!")
+    print()
+    print("15 questions will be presented to you. For each one, you have to answer if the question is true (t) or false (f).")
+    print("There are 3 difficulties. They are easy, medium and hard. Each difficulty has 5 questions total.")
+    print("If you get one question wrong, you're out!")
+    print("Goodluck!")
+    print()
+    while True:
+        if input("Type [y] to start:\n").lower() == "y":
+            break
+
+    with open(r'2. prace_se_soubory\PROJEKT_milionar\quiz_questions.csv', 'r', encoding='utf-8', newline='') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            if row[1] == "easy":
+                easy_questions[row[3]] = row[4]
+            elif row[1] == "medium":
+                medium_questions[row[3]] = row[4]
+            elif row[1] == "hard":
+                hard_questions[row[3]] = row[4]
+    
+    while playing:
+        question_count += 1
+        clear_terminal()
+        print(f"Question no. {question_count}")
+        print("----------------------------")
+        if question_count <= 5:
+            question, answer = random.choice(list(easy_questions.items()))
+        elif question_count > 5 and question_count <= 10:
+            question, answer = random.choice(list(medium_questions.items()))
+        elif question_count > 10:
+            question, answer = random.choice(list(hard_questions.items()))
+        print(question)
+        print()
+        print(f"dbg cheat: {question},{answer}")
+        choice = input("True [t]/False [f]: ")[0].lower()
+        if choice == "t":
+            choice = "True"
+        else:
+            choice = "False"
+        if choice == answer:
+            clear_terminal()
+            print("Correct!")
+            time.sleep(2)
+            pass
+        else:
+            clear_terminal()
+            playing = False
+            print("You lose! Better luck next time.")
+            time.sleep(2)
+        if question_count == 15:
+            should_win = True
+            break
+    
+    if should_win == True:
+        clear_terminal()
+        print("Congrats! You just became a millionaire!")
+        print("Adding your name to the winner's list...")
+        time.sleep(2)
+        print()
+        while True:
+            if input("Type [y] to continue:\n").lower() == "y":
+                break
+        return add_winner(username)
+    else:
+        return logged_in(username)
+            
+
+            
 
 menu()

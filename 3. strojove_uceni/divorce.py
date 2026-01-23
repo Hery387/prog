ec5d9fc0f0bf84c87b13c00377e58b26433bce8f
Hery387 <medved.hery@gmail.com>
2026-01-23 11:22:14 +0100

    divorce

---
+++ 3. strojove_uceni/divorce.py
@@ -0,0 +1,57 @@
+import csv
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+import random
+
+X = []
+Y = []
+
+cesta = r"C:\Users\st025672\Downloads\prog\3. strojove_uceni\data\divorce.csv"
+with open(cesta, "r", encoding="utf-8") as file:
+    for radek in csv.DictReader(file):
+        Y.append(int(radek["Divorce_Y_N"]))
+        random.shuffle(Y)
+        
+        q1 = int(radek["No_home_time"]) 
+        q2 = int(radek["enjoy_travel"])
+        q3 = int(radek["people_goals"])
+        q4 = int(radek["care_sick"])
+        q5 = int(radek["Aggro_argue"])
+        q6 = int(radek["argue_then_leave"])
+
+        X.append([q1, q2, q3, q4, q5, q6])
+
+rows = len(X)
+split = round(0.8 * rows)
+
+X_train, X_test, Y_train, Y_test  = train_test_split(
+        X, Y,
+        test_size=0.2,
+        random_state=42)
+
+
+neuronka = MLPClassifier(
+    hidden_layer_sizes=(19, 15, 9),
+    activation="identity",
+    max_iter=6969,
+    verbose=True
+)
+
+neuronka.fit(X_train, Y_train)
+
+predikce = neuronka.predict(X_test)
+pocet = len(predikce)
+
+spravne = 0
+for i in range(pocet):
+    if predikce[i] == Y_test[i]:
+        spravne += 1
+
+print(f"Poměr správných odpovědí: {round(spravne/pocet*100)}%")
+ConfusionMatrixDisplay.from_predictions(
+    Y_test, predikce)
+plt.show()
+
